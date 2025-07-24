13027209a1d8614e5515490a13f2fe5fe5fa37e3
Sota <sota.matsui@akane.waseda.jp>
2025-07-24 15:55:17 +0900

    fix: ナビバーとページヘッダーが被る問題

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -1,26 +1,24 @@
 export default async function PageHeader({ titleJa, titleEn, desc }: { titleJa: string, titleEn: string, desc?: string }) {
   return (
-    <section className="w-full flex flex-col items-center">
+    <section className="w-full flex flex-col items-center border-t border-foreground/10 mt-24">
       <div className="w-full max-w-[1440px] flex flex-row items-stretch relative">
         <div className="grow-[1] border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed max-lg:hidden [--pattern-fg:var(--foreground)]/10"></div>
         <div className="w-full max-w-7xl">
           <div className="flex flex-col justify-center">
-            <section className="border-t border-b border-foreground/10 w-full max-w-7xl py-8">
+            <section className="border-b border-foreground/10 w-full max-w-7xl py-8">
               <h2 className="text-3xl lg:text-5xl font-light w-full divide-effect">
                 <div className="pl-8">
-                  <p className="font-mono text-sm opacity-60 px-1">
-                    {titleEn}
-                  </p>
+                  <p className="font-mono text-sm opacity-60 px-1">{titleEn}</p>
                   {titleJa}
                 </div>
               </h2>
-              {
-                desc != null ?
-                  <div className="text-md text-foreground pt-8">
-                    <p className="px-8 divide-effect">{desc}</p>
-                  </div>
-                  : <></>
-              }
+              {desc != null ? (
+                <div className="text-md text-foreground pt-8">
+                  <p className="px-8 divide-effect">{desc}</p>
+                </div>
+              ) : (
+                <></>
+              )}
             </section>
           </div>
         </div>
