39aabba08926d34ad980bfabeabca7f5a05d4fb3
Sota <sota.matsui@akane.waseda.jp>
2025-07-24 16:18:11 +0900

    feat: トップページの可読性を向上

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <main className="flex flex-col items-center gap-20">
         <Hero />
-        <section className="w-full flex flex-col items-center divide-effect px-4 lg:px-0">
+        <section className="w-full flex flex-col items-center divide-effect px-4 lg:px-0 bg-background/70">
           <div className="w-full max-w-[1440px] flex flex-row items-stretch relative">
             <div className="grow-[1] border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed max-lg:hidden [--pattern-fg:var(--foreground)]/10"></div>
             <div className="w-full max-w-7xl">
